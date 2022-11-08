207df859a49f0a75922f688655268e4a96186a37
amina-work <mina.works28@gmail.com>
2022-11-09 00:20:39 +0100

    adding nodes topology created a program that: ask user for number of nodes to be created in a graph randomaly generate the coordination of those nodes from an interval of 0 to 100 randomaly generate the size of those from an interval of 100 to 3000 draw and show the graph with the labels of the nodes and a list of their coodinations

--- myapp.py
+++ myapp.py
@@ -2,22 +2,38 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 import networkx as nx
-import math
+import random
+import xlrd #for excel
 
+#initializing graphs, creating multiple for later multiple algorithms testing
+G = nx.Graph()
+G2 = nx.Graph()
 
 #Initializing the number of nodes, 
-def chooseNodesNum(num):
-    G = nx.Graph()
+def createNodes(num):
     n_name = 1 #giving each node a diff name as Graph count similar nodes as one
+    node_sizes = []
     for i in range(num):
-        G.add_node("N" + str(n_name))
+        #setting up the graph zone 100x100. 
+        G.add_node("N" + str(n_name), pos=(random.randint(0, 100), random.randint(0, 100)))
+        G2.add_node("N" + str(n_name), pos=(random.randint(0, 100), random.randint(0, 100)))
         n_name += 1
-    return list(G)
+        #so that each node will have a diff transimittion range from 100 to 3000
+        node_sizes.append(random.randint(1, 30)*100)
+    pos=nx.get_node_attributes(G,'pos')
+    print(pos) #return each node x, y position
+    plt.figure(1)
+    nx.draw(G, pos, node_size=node_sizes, with_labels=True)
+    plt.figure(2)
+    nx.draw(G2, pos, node_size=node_sizes, with_labels=True)
+    plt.show()
+
         
     
 def main():
     nm = int(input("Entrer le nombre de nœuds:"))
-    print(chooseNodesNum(nm))
+    print(createNodes(nm))
+
     
 main()
 #nx.draw(G)
