4a903d8d98d496c5132f3c1e5a6663b2ae6e1d6e
amina-work <mina.works28@gmail.com>
2022-11-29 13:29:34 +0100

    finding nodes neighbors

--- myapp.py
+++ myapp.py
@@ -1,16 +1,14 @@
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-import numpy as np
 import networkx as nx
 import random
-import xlrd #for excel
+import math
 
 #initializing graphs, creating multiple for later multiple algorithms testing
-G = nx.Graph()
-G2 = nx.Graph()
+G = nx.Graph() #Vecteur 
+G2 = nx.Graph() #OLSR
 
-#fixate the transmission range of all nodes
-rayon = 50
+#Generate a random number 
+rayon = random.randint(30, 70)
 
 #Initializing the number of nodes, 
 def createNodes(num):
@@ -19,7 +17,7 @@ def createNodes(num):
     for i in range(num):
         #setting up the graph zone 100x100. 
         G.add_node("N" + str(n_name), pos=(random.randint(0, 100), random.randint(0, 100)))
-        G2.add_node("N" + str(n_name), pos=(random.randint(0, 100), random.randint(0, 100)))
+        #G2.add_node("N" + str(n_name), pos=(random.randint(0, 100), random.randint(0, 100)))
         n_name += 1
     
     #add node position as node attributes
@@ -28,16 +26,29 @@ def createNodes(num):
     
     #appending [x, y] of each node into a list
     list_des_xy = []
+    list_des_noueds = []
     for nd, att in pos.items():
         list_des_xy.append([att[0], att[1]])
+        list_des_noueds.append(nd)
+    print(list_des_noueds)
     print(list_des_xy)
+    for nd in list_des_xy: #resigining each [x, y] to their node
+        for nd2 in list_des_xy:
+            if nd != nd2: #so it doesn"t count the distance with itself
+                print(nd, nd2)
+                distance = math.dist(nd, nd2)
+                print(math.dist(nd, nd2)) #calculating the Euclidean distance
+                if distance < rayon:
+                    print(nd, "et", nd2, "sont des voisins.")
+        
+    print(rayon)
             
     #show the graphs, in diff figures
-    plt.figure(1)
-    nx.draw(G, pos, with_labels=True)
-    plt.figure(2)
-    nx.draw(G2, pos, with_labels=True)
-    plt.show()
+    #plt.figure(1)
+    #nx.draw(G, pos, with_labels=True)
+    #plt.figure(2)
+    #nx.draw(G2, pos, with_labels=True)
+    #plt.show()
 
         
     
